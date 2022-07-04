14fe4194f3c76ac1323fb6e3587466fcb8f73ccf
Tan Yong <asleepychild@gmail.com>
2022-07-04 23:22:53 +0800

    今天先这样,bootstrap遇到了点问题。

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -2,9 +2,16 @@ package cn.itcast.user.web;
 
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @Slf4j
 @RestController
@@ -13,6 +20,15 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    // 取配置文件中的变量值
+    @NacosValue("${pattern.dateformat}")
+    private String dateformat;
+
+    @GetMapping("/now")
+    public String now(){
+        return dateformat;
+    }
 
     /**
      * 路径： /user/110
